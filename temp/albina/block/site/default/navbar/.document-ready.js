517bbfa823d9477475a774cbaf1c9cc591e6bf05
caezar47 <iss47@ya.ru>
2017-11-20 14:37:38 +0300

    ready

--- temp/albina/block/site/default/navbar/.document-ready.js
+++
@@ -1,8 +0,0 @@
-//var url = window.location.pathname;
-var url = window.location.href;
-$('.navbar__nav a[href="'+url+'"]').parent().addClass('is--active'); 
-$('.navbar-category__nav .navbar-category__link[href="'+url+'"]').addClass('is--active'); 
-$('.navbar__nav').on('click', '.navbar__nav-dropdown a[data-toggle="tab-dropdown"]', function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-});
